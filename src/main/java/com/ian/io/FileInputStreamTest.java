89b3dbae57cb38fe9e31b8d38857b03ff419aed6
Ian Zheng <flks_2005@qq.com>
2017-10-09 15:38:46 +0800

    add outputstream ext

--- src/main/java/com/ian/io/FileInputStreamTest.java
+++ src/main/java/com/ian/io/FileInputStreamTest.java
@@ -21,9 +21,25 @@ public class FileInputStreamTest {
                 byte[] bytes = new byte[20];
                 // hasRead返回每次读取的字节数
                 int hasReads;
+
+                // 跳过40字节
+//                inputStream.skip(40);
+                // 当前不支持
+                if (inputStream.markSupported()){
+                    inputStream.mark(50);
+                }
+
+                // 其他read方法, 从缓冲区中读取从len(10)个字节，放到bytes数组中，不过不是从头放，是从offset的位置开始,所以，读取的后三位没有打印出来
+                int tmp = inputStream.read(bytes, 3, 10);
+                System.out.println(new String(bytes, 0, tmp));
+
+
                 // 返回-1代表到了文件的结尾
                 while ((hasReads = inputStream.read(bytes)) > 0){
                     System.out.print(new String(bytes,0,hasReads));
+                    if(inputStream.markSupported()){
+                        inputStream.reset(); // reset到mark位置
+                    }
                 }
         }catch (IOException ioe){
             ioe.printStackTrace();
---
+++ src/main/java/com/ian/io/FileOutputStreamTest.java
@@ -0,0 +1,24 @@
+package com.ian.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamTest {
+
+    public static void main(String[] args) throws IOException {
+
+        try(
+                FileInputStream inputStream = new FileInputStream("src/main/java/com/ian/io/FileOutputStreamTest.java");
+                FileOutputStream outputStream = new FileOutputStream("src/main/java/com/ian/io/FileOutputStreamTest.tmp")
+                ){
+            byte[] bytes = new byte[20];
+            int hasRead;
+            while((hasRead = inputStream.read(bytes)) > 0){
+                outputStream.write(bytes, 0, hasRead);
+            }
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+}
