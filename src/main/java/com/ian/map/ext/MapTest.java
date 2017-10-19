0dbca78e5fb8ecd5b7a7dc1a120b87e3eef6bbef
Ian Zheng <flks_2005@qq.com>
2017-10-19 17:28:39 +0800

    collection ext12

---
+++ src/main/java/com/ian/map/ext/MapTest.java
@@ -0,0 +1,24 @@
+package com.ian.map.ext;
+
+import java.util.*;
+
+public class MapTest {
+
+    public static void main(String[] args) {
+
+        String[] strs = {"a", "b", "a", "b", "c", "a", "b", "c", "b"};
+        List<String> list = Arrays.asList(strs);
+        Map<String, Integer> map = new HashMap<>();
+        long start = System.currentTimeMillis();
+//        for (int i = 0; i < list.size(); i++) {
+//            map.put(list.get(i), Collections.frequency(list, list.get(i)));
+//        }
+        // 下面这种方法更快
+        for (int i = 0; i < list.size(); i++) {
+            map.put(list.get(i), map.get(list.get(i)) == null? 1 : map.get(list.get(i)) + 1);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(map);
+        System.out.println(end - start);
+    }
+}
