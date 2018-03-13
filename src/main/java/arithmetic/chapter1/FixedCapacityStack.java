63f06d9376460f6cc12760e094a06d8625c981e2
Ian Zheng <ian.zheng@sap.com>
2018-03-13 17:56:58 +0800

    correct sample

--- src/main/java/arithmetic/chapter1/FixedCapacityStack.java
+++ src/main/java/arithmetic/chapter1/FixedCapacityStack.java
@@ -1,9 +1,18 @@
 package arithmetic.chapter1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class FixedCapacityStack {
+public class FixedCapacityStack implements Iterable<String> {
 
 	List<String> list = new ArrayList<String>();
+
+	@Override
+	public Iterator<String> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
