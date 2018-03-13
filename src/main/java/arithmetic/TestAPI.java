72e99e80a6fe1844eb70ca3ccecbef4c7adb0fe3
Ian Zheng <ian.zheng@sap.com>
2018-03-13 17:56:43 +0800

    update

---
+++ src/main/java/arithmetic/TestAPI.java
@@ -0,0 +1,9 @@
+package arithmetic;
+
+import edu.princeton.cs.algs4.Graph;
+
+public class TestAPI {
+
+	
+	
+}
---
+++ src/main/java/arithmetic/chapter1/FixedCapacityStack.java
@@ -0,0 +1,9 @@
+package arithmetic.chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedCapacityStack {
+
+	List<String> list = new ArrayList<String>();
+}
